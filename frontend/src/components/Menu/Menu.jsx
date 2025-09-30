3710429a5848cb25f97ac3e47f4431bac1716fb5
Lera <valeria.usatenko@gmail.com>
2025-09-30 10:00:12 +0200

    Frontend: added Header components and routes

---
+++ frontend/src/components/Menu/Menu.jsx
@@ -0,0 +1,38 @@
+import { NavLink } from "react-router-dom";
+import logo from "../../images/logo.png";
+
+export default function Menu() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <NavLink className="navbar-brand" to="/">
+          <img src={logo} alt="Logo" style={{ maxHeight: "50px" }} />
+        </NavLink>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/login">Login</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">About</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
